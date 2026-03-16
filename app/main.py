8b48a0e0dca9aff25262c1bdbe1e63200f75c24c
Yasowant N <yasowant1998@gmail.com>
2026-03-16 16:07:43 +0530

    fix cors middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 from sqlalchemy import text
+from fastapi.middleware.cors import CORSMiddleware
 from app.database.database import engine,Base
 from app.routes.todo_routes import router as todo_router
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
+
+
+origins = [
+    "http://localhost:8080",   # React / Next dev server
+    "http://127.0.0.1:8080",   # Local alternative
+    "https://radiant-list-studio.vercel.app"  # Your deployed frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/db-test")
 def db_test():

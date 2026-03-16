6f6e6192570576de56135389921e969cb82adc1c
Yasowant N <yasowant1998@gmail.com>
2026-03-16 14:13:42 +0530

    updated code

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:yasowant264@localhost:5432/todo_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
@@ -12,11 +13,3 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()

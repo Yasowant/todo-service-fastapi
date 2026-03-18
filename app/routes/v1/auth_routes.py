4b09dac7fbc16f6ba529413a6020a56b5a6ff33f
Yasowant N <yasowant1998@gmail.com>
2026-03-18 14:25:52 +0530

    updated code

--- app/routes/v1/auth_routes.py
+++ app/routes/v1/auth_routes.py
@@ -4,6 +4,8 @@ from app.database.database import get_db
 from app.schemas.user_schema import UserCreate, UserLogin
 from app.services.auth_service import register_user, login_user
 from app.services.email_service import send_verification_email
+from app.utils.dependencies import get_current_user
+from app.models.user_model import User
 
 
 router = APIRouter(
@@ -36,4 +38,13 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
 def logout():
     return{
         "message":"Logout successful.Please remove the token from client storage"
+    }
+
+@router.get("/profile")
+def get_profile(current_user: User = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "name": current_user.name,
+        "email": current_user.email,
+        "is_verified": current_user.is_verified
     }
--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,8 +1,12 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
+from sqlalchemy.orm import Session
 import os
 from dotenv import load_dotenv
+
+from app.database.database import get_db
+from app.models.user_model import User
 
 load_dotenv()
 
@@ -12,7 +16,10 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
@@ -24,7 +31,16 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
                 detail="Invalid token"
             )
 
-        return user_id   # ✅ RETURN AS STRING (UUID)
+        # ✅ fetch full user from DB
+        user = db.query(User).filter(User.id == int(user_id)).first()
+
+        if not user:
+            raise HTTPException(
+                status_code=404,
+                detail="User not found"
+            )
+
+        return user  # ✅ RETURN FULL USER
 
     except JWTError:
         raise HTTPException(
